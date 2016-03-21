973b4360c9098d1904a9022cff6c8311377c805d
Ernest <ernest@megahard.info>
2016-03-21 17:42:36 +0700

    start

---
+++ Assets/Keystore Helper/Editor/AndroidKeystoreLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+#if UNITY_EDITOR
+
+[InitializeOnLoad]
+public class AndroidKeystoreLoader
+{
+	static AndroidKeystoreLoader()
+	{
+		string keystorePass, keyaliasName, keyaliasPass;
+		
+		keystorePass = AndroidKeystoreWindow.ReadPrefs(AndroidKeystoreWindow.KEYSTOREPASS);
+		keyaliasName = AndroidKeystoreWindow.ReadPrefs(AndroidKeystoreWindow.KEYALIASNAME);
+		keyaliasPass = AndroidKeystoreWindow.ReadPrefs(AndroidKeystoreWindow.KEYALIASPASS);
+		
+		PlayerSettings.Android.keystorePass = keystorePass;
+		PlayerSettings.Android.keyaliasName = keyaliasName;
+		PlayerSettings.Android.keyaliasPass = keyaliasPass;		
+	}
+}
+
+#endif
